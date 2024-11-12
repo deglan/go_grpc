b733146f5535cce1b39816a582769760383ce6b7
jzgorzaly <zgorzalyj@gmail.com>
2024-11-12 20:12:55 +0100

    initial commit

---
+++ greet/greet_client/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"go_grpc/greet/greetpb"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	fmt.Println("Hello World")
+
+	conn, err := grpc.NewClient("0.0.0.0:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("Failed to dial: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := greetpb.NewGreetServiceClient(conn)
+
+	fmt.Printf("Created client: %v", c)
+
+}
